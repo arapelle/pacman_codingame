e7f4dd2c287587d0b929dbaff7af905b58c36dd3
Aymeric Pellé <aymeric.pelle@gmail.com>
2020-05-15 14:28:51 +0200

    Position_treated stored treated positions. The condition can stop when a given number of positions is treated.

--- grid/exploration/stop_condition/positions_treated.hpp
+++ grid/exploration/stop_condition/positions_treated.hpp
@@ -8,22 +8,27 @@ template <class StopConditionBase = No_stop_condition>
 class Positions_treated : public StopConditionBase
 {
 public:
-    Positions_treated() {}
+    Positions_treated()
+        : number_of_wanted_positions_(std::numeric_limits<std::size_t>::max())
+    {}
 
     explicit Positions_treated(const Position& position)
         : positions_to_treat_{ position }
+        , number_of_wanted_positions_(std::numeric_limits<std::size_t>::max())
     {}
 
     explicit Positions_treated(const std::vector<Position>& positions)
         : positions_to_treat_(positions)
+        , number_of_wanted_positions_(std::numeric_limits<std::size_t>::max())
     {}
 
     explicit Positions_treated(const std::initializer_list<Position>& positions)
         : positions_to_treat_(positions)
+        , number_of_wanted_positions_(std::numeric_limits<std::size_t>::max())
     {}
 
     template <typename MarkGrid>
-    inline bool operator()(const MarkGrid& mark_grid) const
+    inline bool operator()(const MarkGrid& mark_grid)
     {
         for (auto iter = positions_to_treat_.begin(); iter != positions_to_treat_.end(); )
         {
@@ -39,15 +44,19 @@ public:
             }
         }
 
-        return positions_to_treat_.empty() || static_cast<const StopConditionBase&>(*this)(mark_grid);
+        return positions_to_treat_.empty()
+                || treated_positions_.size() == number_of_wanted_positions_
+                || static_cast<const StopConditionBase&>(*this)(mark_grid);
     }
 
     inline const std::vector<Position>& positions_to_treat() const { return positions_to_treat_; }
     inline std::vector<Position>& positions_to_treat() { return positions_to_treat_; }
-
     inline const std::vector<Position>& treated_positions() const { return treated_positions_; }
+    inline std::size_t number_of_wanted_positions() const { return number_of_wanted_positions_; }
+    inline void set_number_of_wanted_positions(std::size_t count) { number_of_wanted_positions_ = count; }
 
 private:
-    mutable std::vector<Position> positions_to_treat_;
-    mutable std::vector<Position> treated_positions_;
+    std::vector<Position> positions_to_treat_;
+    std::vector<Position> treated_positions_;
+    std::size_t number_of_wanted_positions_;
 };
