d1b6fa62843cf1042d131d75eded65ae27e561ba
Aymeric Pellé <aymeric.pelle@gmail.com>
2020-05-17 18:30:54 +0200

    World can test neighbours of a square.

--- game/world.hpp
+++ game/world.hpp
@@ -2,6 +2,7 @@
 
 #include "square.hpp"
 #include "grid/grid.hpp"
+#include "direction/neighbourhood.hpp"
 #include <istream>
 
 class Game;
@@ -24,6 +25,21 @@ public:
     inline const std::vector<Iterator>& small_pellets_iters() const { return small_pellets_iters_; }
 
     void update_from_turn_info(const Turn_info& turn_info);
+
+    template <class Predicate>
+    bool any_neighbour_square_if(const Position& position, Predicate predicate) const
+    {
+        for (const Position& npos : neighbours4(position))
+        {
+            if (contains(npos))
+            {
+                const Square& nsquare = get(npos);
+                if (predicate(nsquare))
+                    return true;
+            }
+        }
+        return false;
+    }
 
 private:
     void remove_all_pellet_();
